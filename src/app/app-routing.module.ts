0e28557b88b0dc6079f443a426d34630c8c22a30
kate-yatsenko <yatsenko.km@gmail.com>
2019-10-30 14:47:55 +0200

    set up initial structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SignInPageComponent } from './modules/pages/sign-in-page/sign-in-page.component';
+import { AlbumsPageComponent } from './modules/pages/albums-page/albums-page.component';
+import { PhotosPageComponent } from './modules/pages/photos-page/photos-page.component';
+import { AuthGuard } from './core/guards/auth.guard';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: SignInPageComponent },
+  { path: 'albums', canActivate: [AuthGuard], component: AlbumsPageComponent },
+  { path: 'albums/:albumId', canActivate: [AuthGuard], component: PhotosPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,34 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignInPageComponent } from './modules/pages/sign-in-page/sign-in-page.component';
+import { AlbumsPageComponent } from './modules/pages/albums-page/albums-page.component';
+import { PhotosPageComponent } from './modules/pages/photos-page/photos-page.component';
+import { HeaderComponent } from './core/header/header.component';
+import { SignInFormComponent } from './modules/components/sign-in-form/sign-in-form.component';
+import { HttpService } from './core/services/http.service';
+import { AuthService } from './core/services/auth.service';
+import { AuthGuard } from './core/guards/auth.guard';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInPageComponent,
+    AlbumsPageComponent,
+    PhotosPageComponent,
+    HeaderComponent,
+    SignInFormComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    HttpService,
+    AuthService,
+    AuthGuard
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
